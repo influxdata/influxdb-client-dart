2d07d08583b9b22c1a1c2b47e4e62fefd63eee2d
Jakub Bednar <jakub.bednar@gmail.com>
2022-11-30 15:12:41 +0100

    chore: update generated `PingApi`

--- lib/api/ping_api.dart
+++ lib/api/ping_api.dart
@@ -13,7 +13,9 @@ class PingApi {
 
   final ApiClient apiClient;
 
-  /// Checks the status of InfluxDB instance and version of InfluxDB.
+  /// Get the status of the instance
+  ///
+  /// Retrieves the status and InfluxDB version of the instance.  Use this endpoint to monitor uptime for the InfluxDB instance. The response returns a HTTP `204` status code to inform you the instance is available.  #### InfluxDB Cloud  - Isn't versioned and doesn't return `X-Influxdb-Version` in the headers.  #### Related guides  - [Influx ping](https://docs.influxdata.com/influxdb/latest/reference/cli/influx/ping/)
   ///
   /// Note: This method returns the HTTP [Response].
   Future<Response> getPingWithHttpInfo() async {
@@ -45,7 +47,9 @@ class PingApi {
     );
   }
 
-  /// Checks the status of InfluxDB instance and version of InfluxDB.
+  /// Get the status of the instance
+  ///
+  /// Retrieves the status and InfluxDB version of the instance.  Use this endpoint to monitor uptime for the InfluxDB instance. The response returns a HTTP `204` status code to inform you the instance is available.  #### InfluxDB Cloud  - Isn't versioned and doesn't return `X-Influxdb-Version` in the headers.  #### Related guides  - [Influx ping](https://docs.influxdata.com/influxdb/latest/reference/cli/influx/ping/)
   Future<void> getPing() async {
     final response = await getPingWithHttpInfo();
     if (response.statusCode >= HttpStatus.badRequest) {
@@ -53,7 +57,9 @@ class PingApi {
     }
   }
 
-  /// Checks the status of InfluxDB instance and version of InfluxDB.
+  /// Get the status of the instance
+  ///
+  /// Returns the status and InfluxDB version of the instance.  Use this endpoint to monitor uptime for the InfluxDB instance. The response returns a HTTP `204` status code to inform you the instance is available.  #### InfluxDB Cloud  - Isn't versioned and doesn't return `X-Influxdb-Version` in the headers.  #### Related guides  - [Influx ping](https://docs.influxdata.com/influxdb/latest/reference/cli/influx/ping/)
   ///
   /// Note: This method returns the HTTP [Response].
   Future<Response> headPingWithHttpInfo() async {
@@ -85,7 +91,9 @@ class PingApi {
     );
   }
 
-  /// Checks the status of InfluxDB instance and version of InfluxDB.
+  /// Get the status of the instance
+  ///
+  /// Returns the status and InfluxDB version of the instance.  Use this endpoint to monitor uptime for the InfluxDB instance. The response returns a HTTP `204` status code to inform you the instance is available.  #### InfluxDB Cloud  - Isn't versioned and doesn't return `X-Influxdb-Version` in the headers.  #### Related guides  - [Influx ping](https://docs.influxdata.com/influxdb/latest/reference/cli/influx/ping/)
   Future<void> headPing() async {
     final response = await headPingWithHttpInfo();
     if (response.statusCode >= HttpStatus.badRequest) {
